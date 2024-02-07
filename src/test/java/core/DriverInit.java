b251d279bc60354860e8d727eff4fe3b84d503d9
BeautifulRus <blue.sky.2021@bk.ru>
2024-02-07 19:26:50 +0500

    RefactorEnd

--- src/test/java/core/DriverInit.java
+++ src/test/java/core/DriverInit.java
@@ -1,7 +1,11 @@
 package core;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -51,12 +55,23 @@ abstract public class DriverInit {
 
 
     private void initLocalDriver() {
-        driver = new ChromeDriver();
+        if ("firefox".equalsIgnoreCase(System.getProperty("type.browser"))) {
+            driver = new FirefoxDriver();
+        }else
+        if ("chrome".equalsIgnoreCase(System.getProperty("type.browser"))){
+            driver = new ChromeDriver();
+        }else
+        if ("opera".equalsIgnoreCase(System.getProperty("type.browser"))){
+            driver = new OperaDriver();// опера кажется не стыкована с драйвером. Не работает
+        }else
+        if ("edge".equalsIgnoreCase(System.getProperty("type.browser"))){
+            driver = new EdgeDriver();
+        }else Assertions.fail("I don't work with that browser. Please change the variable |type.browser| from file |application.properties| to the prescribed option in the code");
 
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10), Duration.ofMillis(500));
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+            wait = new WebDriverWait(driver, Duration.ofSeconds(10), Duration.ofMillis(500));
 
 
 
